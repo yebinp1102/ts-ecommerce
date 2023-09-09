4be8ee4cb58c308b4bc8de54577a5bce09e49ffd
yebin Park <yebinp1102@naver.com>
2023-09-09 15:40:59 +0900

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+
+      <header>E-commerce App</header>
+      <main></main>
+      <footer>All right reserved.</footer>
+
+    </div>
+  )
+}
+
+export default App
