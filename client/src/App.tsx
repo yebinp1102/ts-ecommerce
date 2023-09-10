7107d3a7f58fc1ccc28c043d690770a14b53cc55
yebin Park <yebinp1102@naver.com>
2023-09-10 14:42:39 +0900

    Product Lists

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
 
   return (
-    <div>
+    <div className='d-flex flex-column vh-100'>
 
       <header>E-commerce App</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map(product => (
+            <li key={product.slug}>
+              <img src={product.image} alt={product.name} className='product-image' />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved.</footer>
 
     </div>
