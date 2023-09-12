dd6f41a6f4b630ec1ff84d2a51f2550f890c2014
yebin Park <yebinp1102@naver.com>
2023-09-12 18:48:20 +0900

    Feat : context for swtich dark mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -15,6 +15,7 @@ import axios from 'axios'
 import { Provider } from 'react-redux'
 import {HelmetProvider} from 'react-helmet-async';
 import { store } from './store/store.ts'
+import { StoreProvider } from './context/store.tsx'
 
 axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/';
 
@@ -29,10 +30,12 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <HelmetProvider>
-        <RouterProvider router={router} />
-      </HelmetProvider>
-    </Provider>
+    <StoreProvider>
+      <Provider store={store}>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </Provider>
+    </StoreProvider>
   </React.StrictMode>,
 )
